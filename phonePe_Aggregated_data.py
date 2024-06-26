d86900213b3464ed8d00aebff3b41ee62788d949
J Saravanan <160335056+Jsaravanan5@users.noreply.github.com>
2024-06-26 08:09:00 +0000

    version 1.2

--- phonePe_Aggregated_data.py
+++ phonePe_Aggregated_data.py
@@ -16,7 +16,7 @@ for ins_states in agg_ins_path:
 
     for ins_years in agg_ins_years_path:
         agg_ins_file_path=os.listdir("/workspaces/Phonepe_Pulse_Data/pulse/data/aggregated/insurance/country/india/state/"+ins_states+"/"+ins_years+"/")
-        print(agg_ins_file_path)
+        #print(agg_ins_file_path)
 
         for file in agg_ins_file_path:
             with open("/workspaces/Phonepe_Pulse_Data/pulse/data/aggregated/insurance/country/india/state/"+ins_states+"/"+ins_years+"/"+file,"r") as json_file :
@@ -24,4 +24,12 @@ for ins_states in agg_ins_path:
                 #print(json_file)
           
 
-print(agg_json_ins_file)
+for data in agg_json_ins_file['data']['transactionData']:
+    agg_ins_txn_type=data['name']
+    agg_ins_txn_count=data['paymentInstruments'][0]['count']
+    agg_ins_txn_amount=data['paymentInstruments'][0]['amount']
+
+
+
+
+
