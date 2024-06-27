d696b36db01a56bee9b95e138666c351511634cc
J Saravanan <160335056+Jsaravanan5@users.noreply.github.com>
2024-06-27 11:56:51 +0000

    Version 1.3

--- Plotly_Geoplot.py
+++ Plotly_Geoplot.py
@@ -1,8 +1,40 @@
-import plotly.express as px
-df = px.data.gapminder().query("year == 2007")
-fig = px.scatter_geo(df, locations="iso_alpha",
-                     color="continent", # which column to use to set the color of markers
-                     hover_name="country", # column added to hover information
-                     size="pop", # size of markers
-                     projection="natural earth")
-fig.show()
+
+
+#Map data >> Transaction
+
+import os
+import json as js
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+map_trx_path=os.listdir("/workspaces/Phonepe_Pulse_Data/pulse/data/map/transaction/hover/country/india/state/")
+#print(map_trx_path)
+
+for map_trx_states in map_trx_path:
+    map_trx_years_path=os.listdir("/workspaces/Phonepe_Pulse_Data/pulse/data/map/transaction/hover/country/india/state/"+map_trx_states+"/")
+    #print(map_trx_years_path)
+    
+    for map_trx_years in map_trx_years_path:
+        map_trx_file_path=os.listdir("/workspaces/Phonepe_Pulse_Data/pulse/data/map/transaction/hover/country/india/state/"+map_trx_states+"/"+map_trx_years+"/")
+        #print(map_trx_file_path) 
+
+        for map_trx_file in map_trx_file_path:
+            with open("/workspaces/Phonepe_Pulse_Data/pulse/data/map/transaction/hover/country/india/state/"+map_trx_states+"/"+map_trx_years+"/"+map_trx_file,"r")as map_json_file:
+                map_trx_json_data=js.load(map_json_file)    
+                #print(map_trx_json_data)
+                map_dist=[]
+                for i in map_trx_json_data['data']['hoverDataList']:   #[0]['metric']:
+                    map_trx_dist=[i][0]['name']
+                    #print(map_trx_dist)
+                    map_trx_count=[i][0]['metric'][0]['count']
+                    print(map_trx_count)
+                    map_trx_amount=[i][0]['metric'][0]['amount']
+                    map_trx_amt=round(map_trx_amount,2)
+                    print(map_trx_amount)
+
+
+
+
+
+                    
