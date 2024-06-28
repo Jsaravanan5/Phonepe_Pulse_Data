b137f5ede06f3f7dcd991bf23c4af97a72f8f94f
J Saravanan <160335056+Jsaravanan5@users.noreply.github.com>
2024-06-28 10:47:13 +0000

    Version 1.4

--- phonePe_Top_data.py
+++ phonePe_Top_data.py
@@ -1,4 +1,24 @@
+#import Library:
+import os
+import json as js
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# Top_data:
 
-#
+# Top_data >> Transaction:
+
+
+top_trx_path=os.listdir("/workspaces/Phonepe_Pulse_Data/pulse/data/top/transaction/country/india/state/")
+#print(top_trx_path)
+
+
+
+for top_trx_states in top_trx_path:
+    top_trx_years_path=os.listdir("/workspaces/Phonepe_Pulse_Data/pulse/data/top/transaction/country/india/state"+"/"+top_trx_states+"/")
+    #print(top_trx_years_path)
+
+    for top_trx_years in top_trx_years_path:
+        top_trx_file_path=os.listdir("/workspaces/Phonepe_Pulse_Data/pulse/data/top/transaction/country/india/state"+"/"+top_trx_states+"/"+top_trx_years+"/")
+        #print(top_trx_file_path)
+        
